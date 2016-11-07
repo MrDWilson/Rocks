50e228248ba6b6c37f78f283180bc787f1f7129b
MyForteIsTimeTravel <RyanNeedham@outlook.com>
2016-11-07 16:10:33 +0000

    Removed a world of redundant code and simplified. Now drops pseudo-asteroids or varying sizes and positions.

--- SnowboardingGamePrototype/GameElements.swift
+++ SnowboardingGamePrototype/GameElements.swift
@@ -8,152 +8,56 @@
 
 import SpriteKit
 
+// Collision Bit Mask
 struct collision {
     static let player:UInt32 = 0x00
-    static let obstacle:UInt32 = 0x01
-}
-
-enum obstacleType: Int {
-    case small  = 0
-    case medium = 1
-    case large  = 2
-}
-
-enum RowType: Int {
-    case oneSmall   = 0
-    case oneMedium  = 1
-    case oneLarge   = 2
-    case twoSmall   = 3
-    case twoMedium  = 4
-    case threeSmall = 5
+    static let asteroid:UInt32 = 0x01
 }
 
 extension GameScene {
 
-    /** 
-     *  addPlayer
-     */
-    func addPlayer () {
-        // sprite, position and name
-        player = SKSpriteNode(color: UIColor.red, size: CGSize(width: 50, height: 50))
-        player.position = CGPoint(x: self.size.width / 2 , y: 220)
-        player.name = "PlayerOne"
+    func spawnPlayer () {
+        player = SKSpriteNode(color: UIColor.gray, size: CGSize(width: 24, height: 24))
         
-        // physics
+        // Give name and position
+        player.name = "PlayerOne"
+        player.position = CGPoint(x: self.size.width / 2 , y: 60)
+        
+        // Give physics
         player.physicsBody?.isDynamic = false
         player.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: player.size.width, height: player.size.height))
         player.physicsBody?.categoryBitMask = collision.player
         player.physicsBody?.collisionBitMask = 0
-        player.physicsBody?.contactTestBitMask = collision.obstacle
+        player.physicsBody?.contactTestBitMask = collision.asteroid
         
+        // add to scene
         addChild(player);
-        
-        initPos = player.position
     }
     
-    func addObstacle (type: obstacleType) -> SKSpriteNode {
-        let obstacle = SKSpriteNode(color: UIColor.white, size: CGSize(width: 0, height: 30))
-    
-        obstacle.name = "obstacle"
-        obstacle.physicsBody?.isDynamic = true
-        
-        switch type {
-            case.small:
-                obstacle.size.width = self.size.width * 0.2
-                break
-            case.medium:
-                obstacle.size.width = self.size.width * 0.35
-                break
-            case.large:
-                obstacle.size.width = self.size.width * 0.75
-                break
-        }
-        
-        obstacle.position = CGPoint(x: 0, y: self.size.height + obstacle.size.height)
-        obstacle.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: obstacle.size.width, height: obstacle.size.height))
-        obstacle.physicsBody?.categoryBitMask = collision.obstacle
-        obstacle.physicsBody?.collisionBitMask = 0
-        
-        
-        return obstacle
-    }
-    
-    func addMovement (obstacle: SKSpriteNode) {
+    func spawnAsteroid () {
         var actionArray = [SKAction]()
+        let asteroid    = SKSpriteNode(color: UIColor.brown, size: CGSize(width: 10, height: 10))
+        let speed       = 5
+        let size        = Int(arc4random_uniform(14))
+
+        // Give name, size and position
+        asteroid.name = "asteroid"
+        asteroid.size.width = asteroid.size.width * CGFloat(size)
+        asteroid.size.height = asteroid.size.height * CGFloat(size)
+        asteroid.position = CGPoint(x: CGFloat(Int(arc4random_uniform(UInt32(self.size.width)))), y: self.size.height + asteroid.size.height)
+
+        // Give physics components
+        asteroid.physicsBody?.isDynamic = true
+        asteroid.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: asteroid.size.width, height: asteroid.size.height))
+        asteroid.physicsBody?.categoryBitMask = collision.asteroid
+        asteroid.physicsBody?.collisionBitMask = 0
         
-        actionArray.append(SKAction.move(to: CGPoint(x: obstacle.position.x, y: -obstacle.size.height), duration: 5))
+        // Give motion
+        actionArray.append(SKAction.move(to: CGPoint(x: asteroid.position.x, y: -asteroid.size.height), duration: TimeInterval(speed)))
         actionArray.append(SKAction.removeFromParent());
-        
-        obstacle.run(SKAction.sequence(actionArray))
-        
+        asteroid.run(SKAction.sequence(actionArray))
+       
+        // add to scene
+        addChild(asteroid)
     }
-    
-    func addRow (type: RowType) {
-        switch type {
-            case .oneSmall:
-                let obst = addObstacle(type: .small)
-                obst.position = CGPoint(x: self.size.width / 2, y: obst.position.y)
-                addMovement(obstacle: obst)
-                addChild(obst)
-                break
-            case .oneMedium:
-                let obst = addObstacle(type: .medium)
-                obst.position = CGPoint(x: self.size.width / 2, y: obst.position.y)
-                addMovement(obstacle: obst)
-                addChild(obst)
-                break
-            case .oneLarge:
-                let obst = addObstacle(type: .large)
-                obst.position = CGPoint(x: self.size.width / 2, y: obst.position.y)
-                addMovement(obstacle: obst)
-                addChild(obst)
-                break
-            case .twoSmall:
-                let obst1 = addObstacle(type: .small)
-                let obst2 = addObstacle(type: .small)
-                
-                obst1.position = CGPoint(x: obst1.size.width + 50, y: obst1.position.y)
-                obst2.position = CGPoint(x: self.size.width - obst2.size.width - 50, y: obst2.position.y)
-            
-                addMovement(obstacle: obst1)
-                addMovement(obstacle: obst2)
-                
-                addChild(obst1)
-                addChild(obst2)
-            
-                break
-            case .twoMedium:
-                let obst1 = addObstacle(type: .medium)
-                let obst2 = addObstacle(type: .medium)
-                
-                obst1.position = CGPoint(x: obst1.size.width / 2 + 50, y: obst1.position.y)
-                obst2.position = CGPoint(x: self.size.width - obst2.size.width / 2 - 50, y: obst2.position.y)
-                
-                addMovement(obstacle: obst1)
-                addMovement(obstacle: obst2)
-                
-                addChild(obst1)
-                addChild(obst2)
-                break
-            case .threeSmall:
-                let obst1 = addObstacle(type: .small)
-                let obst2 = addObstacle(type: .small)
-                let obst3 = addObstacle(type: .small)
-                
-                obst1.position = CGPoint(x: obst1.size.width / 2 + 50, y: obst1.position.y) // left
-                obst2.position = CGPoint(x: self.size.width - obst2.size.width / 2 - 50, y: obst2.position.y) // right
-                obst3.position = CGPoint(x: self.size.width / 2, y: obst2.position.y) // center
-                
-                addMovement(obstacle: obst1)
-                addMovement(obstacle: obst2)
-                addMovement(obstacle: obst3)
-                
-                addChild(obst1)
-                addChild(obst2)
-                addChild(obst3)
-                break
-        }
-    }
-    
-    
 }
--- SnowboardingGamePrototype/GameScene.swift
+++ SnowboardingGamePrototype/GameScene.swift
@@ -10,64 +10,21 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene, SKPhysicsContactDelegate {
+    var lastYieldTimeInterval = TimeInterval()
+    var lastUpdateInterval = TimeInterval()
     
     var player:  SKSpriteNode!
-    var initPos: CGPoint!
-    
-    func resetPlayerPosition () {
-        player.position = initPos
-    }
-    
+
+    // On-Start
     override func didMove(to view: SKView) {
         physicsWorld.gravity = CGVector (dx: 0.0, dy: 0.0)
         physicsWorld.contactDelegate = self
         
-        addPlayer()
-        addRow(type: RowType.twoSmall);
+        spawnPlayer()
     }
     
-    func addRandomRow () {
-        let rand = Int(arc4random_uniform(6))
-        
-        addRow(type: RowType(rawValue: rand)!)
-        
-        switch(rand) {
-            case 0:
-                addRow(type: RowType(rawValue: 0)!)
-                break
-            case 1:
-                addRow(type: RowType(rawValue: 1)!)
-                break
-            case 2:
-                addRow(type: RowType(rawValue: 2)!)
-                break
-            case 3:
-                addRow(type: RowType(rawValue: 3)!)
-                break
-            case 4:
-                addRow(type: RowType(rawValue: 4)!)
-                break
-            case 5:
-                addRow(type: RowType(rawValue: 5)!)
-                break
-            default:
-                break
-        }
-    }
-    
-    var lastUpdateInterval = TimeInterval()
-    var lastYieldTimeInterval = TimeInterval()
-    
-    func updateWithLast (lastUpdate: CFTimeInterval) {
-        lastYieldTimeInterval += lastUpdate
-        if (lastYieldTimeInterval > 0.6) {
-            lastYieldTimeInterval = 0
-            addRandomRow()
-        }
-    }
-    
+    // On-Update
     override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
         var timeSinceLastUpdate = currentTime - lastUpdateInterval
         lastUpdateInterval = currentTime
         
@@ -77,12 +34,21 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         }
         
         updateWithLast(lastUpdate: timeSinceLastUpdate)
-        
     }
     
+    // On-Update delegate
+    func updateWithLast (lastUpdate: CFTimeInterval) {
+        lastYieldTimeInterval += lastUpdate
+        if (lastYieldTimeInterval > 0.6) {
+            lastYieldTimeInterval = 0
+            spawnAsteroid()
+        }
+    }
+    
+    // On-Collision
     func didBegin(_ contact: SKPhysicsContact) {
         if (contact.bodyA.node?.name == "PlayerOne") {
-            print("HIT");
+            print("COLLISION - GAME OVER");
         }
     }
 }
