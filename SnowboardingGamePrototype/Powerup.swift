de5c13e72499010930a0bca1e90d580d351c6e4a
Ryan Needham <RyanNeedham@outlook.com>
2016-11-11 00:23:21 +0000

    Fixed some bugs, tidied some code

--- SnowboardingGamePrototype/Powerup.swift
+++ SnowboardingGamePrototype/Powerup.swift
@@ -1,3 +1,12 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *  Powerup.swift
+ *  Space Game
+ *
+ *  Created by Ryan Needham & Danny Wilson on 07/11/2016.
+ *  Copyright © 2016 Ryan Needham & Danny Wilson.
+ *  All rights reserved.
+ *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 import SpriteKit
 
 extension GameScene {
