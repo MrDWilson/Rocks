44797591f616f1d3aa1d75a6dfde9263868255c0
Ryan Needham <RyanNeedham@outlook.com>
2016-11-13 13:51:29 +0000

    Merging Explosion Branch

--- SnowboardingGamePrototype/Powerup.swift
+++ SnowboardingGamePrototype/Powerup.swift
@@ -14,45 +14,70 @@ extension GameScene {
         private enum PowerupType {
             case HealthPickup
             case AmmoPickup
-            case PointsPickup
+            case PointsPickup_25
+            case PointsPickup_50
+            case PointsPickup_100
         }
     
-        private let sprite  = SKSpriteNode(color: UIColor.red, size: CGSize(width: 15, height: 15))
-        private var actions = [SKAction]()
-        private let speed   = 5
-        private var type    = PowerupType.PointsPickup // default to points powerup
+        private let sprite            = SKSpriteNode(color: UIColor.red, size: CGSize(width: 15, height: 15))
+        private let velocity:CGVector = CGVector(dx: 0, dy: 0 - 4)
+        private var xConfine:Int      = 1080
+        private var yConfine:Int      = 1920 // 6 plus default
         
-        func spawn (x: Int, y: Int, texture: Cache) -> SKSpriteNode {
-            
-            // Give name, size and position
-            sprite.position = CGPoint(x: x, y: y)
-    
+        private var textureCache: TextureCache!
+        
+        func setXConfine (con: Int) { xConfine = con - Int(sprite.size.width) }
+        func setYConfine (con: Int) { yConfine = con + Int(sprite.size.width) }
+        
+        private var type    = PowerupType.PointsPickup_25 // default to points powerup
+
+        func generateType () {
             // decide type
-            let ting = arc4random_uniform(3)
+            let ting = arc4random_uniform(5)
             switch ting {
                 case 0:
                     sprite.name = "HealthPickup"
                     type = .HealthPickup
-                    sprite.texture = texture.getCached(key: "health")
-                    let changeColorAction = SKAction.colorize(with: UIColor.red, colorBlendFactor: 1.0, duration: 0)
-                    sprite.run(changeColorAction)
+                    sprite.texture = textureCache.getCached(key: "HealthPickup")
+                    sprite.run(SKAction.colorize(with: UIColor.red, colorBlendFactor: 1.0, duration: 0))
                     break
                 case 1:
                     sprite.name = "AmmoPickup"
                     type = .AmmoPickup
-                    sprite.texture = texture.getCached(key: "ammo")
-                    let changeColorAction = SKAction.colorize(with: UIColor.cyan, colorBlendFactor: 1.0, duration: 0)
-                    sprite.run(changeColorAction)
+                    sprite.texture = textureCache.getCached(key: "AmmoPickup")
+                    sprite.run(SKAction.colorize(with: UIColor.cyan, colorBlendFactor: 1.0, duration: 0))
                     break
                 case 2:
-                    sprite.name = "PointsPickup"
-                    type = .PointsPickup
-                    sprite.texture = texture.getCached(key: "points")
+                    sprite.name = "PointsPickup_25"
+                    type = .PointsPickup_25
+                    sprite.texture = textureCache.getCached(key: "PointsPickup_1")
+                    sprite.run(SKAction.colorize(with: UIColor.gray, colorBlendFactor: 1.0, duration: 0))
+                    break
+                case 3:
+                    sprite.name = "PointsPickup_50"
+                    type = .PointsPickup_50
+                    sprite.texture = textureCache.getCached(key: "PointsPickup_2")
+                    sprite.run(SKAction.colorize(with: UIColor.lightGray, colorBlendFactor: 1.0, duration: 0))
+                    break
+                case 4:
+                    sprite.name = "PointsPickup_100"
+                    type = .PointsPickup_100
+                    sprite.texture = textureCache.getCached(key: "PointsPickup_3")
+                    sprite.run(SKAction.colorize(with: UIColor.white, colorBlendFactor: 1.0, duration: 0))
                     break
                 default:
                     break
             }
+        }
+        
+        func spawn (textureCache: TextureCache) -> SKSpriteNode {
+            self.textureCache = textureCache
             
+            generateType()
+            
+            // Give position
+            sprite.position.x = CGFloat(arc4random_uniform(UInt32(xConfine)))
+            sprite.position.y = CGFloat(yConfine + Int(arc4random_uniform(1024)))
             
             sprite.size.width = 20
             sprite.size.height = 20
@@ -60,21 +85,48 @@ extension GameScene {
             // Give physics components
             sprite.physicsBody?.isDynamic = true
             sprite.physicsBody = SKPhysicsBody(rectangleOf: sprite.size)
-            sprite.physicsBody?.categoryBitMask = collision.powerup
-            sprite.physicsBody?.collisionBitMask = collision.player
-            sprite.physicsBody?.contactTestBitMask = collision.powerup
-            sprite.physicsBody?.density = 0.001 // fix hitting powerups side on
+            sprite.physicsBody?.categoryBitMask = powerupCollisionCat
+            sprite.physicsBody?.contactTestBitMask = playerCollisionCat
+            sprite.physicsBody?.collisionBitMask = playerCollisionCat
+            sprite.physicsBody?.mass = 0.0000001
             
-            // Give motion
-            //actionArray.append(SKAction.rotate(byAngle: 360, duration: TimeInterval(spin)))
-            actions.append (SKAction.move(to: CGPoint(x: sprite.position.x, y: -sprite.size.height), duration: TimeInterval(speed)))
-            actions.append (SKAction.removeFromParent()); // ISSUE: Asteroids aren't removing themselves to be garbage collected
-            sprite.run     (SKAction.sequence(actions))
+            sprite.physicsBody?.density = 0.0000001 // fix hitting powerups side on
             
             sprite.zPosition = -2
             
             // add to scene
             return sprite
+        }
+        
+        func update () {
+            sprite.position.x += velocity.dx
+            sprite.position.y += velocity.dy
+            
+//          print("x: " + String(describing: sprite.position.x))
+//          print("y: " + String(describing: sprite.position.y))
+            
+            if (sprite.position.y < 0 - sprite.size.height) {
+                reuse()
+            }
+            
+            sprite.physicsBody?.allContactedBodies().forEach {
+                if ($0.node?.name == "player") {
+                    collected()
+                }
+            }
+        }
+        
+        func collected () {
+            reuse()
+        }
+        
+        private func reuse () {
+            // regenerate
+            generateType()
+            
+            // reuse
+            sprite.position.x = CGFloat(arc4random_uniform(UInt32(xConfine)))
+            sprite.position.y = CGFloat(yConfine + Int(arc4random_uniform(200)))
         }
     }
 }
