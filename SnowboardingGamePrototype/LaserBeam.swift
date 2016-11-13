4e0550d850daf53dbc2b65ea118e0c39e3754db1
Ryan Needham <RyanNeedham@outlook.com>
2016-11-13 13:49:59 +0000

    Mergin Explosion Branch

--- SnowboardingGamePrototype/LaserBeam.swift
+++ SnowboardingGamePrototype/LaserBeam.swift
@@ -11,14 +11,17 @@ import SpriteKit
 
 extension GameScene {
     class LaserBeam {
-        private let sprite = SKSpriteNode(color: UIColor.cyan , size: CGSize(width: 0.5, height: 10))
-        private var origin = CGVector(dx: 0, dy: 0)
-        private var path = CGVector(dx: 0, dy: 10)
+        private let sprite = SKSpriteNode (color: UIColor.cyan , size: CGSize(width: 0.5, height: 10))
+        private var origin = CGVector     (dx: 0, dy: 0)
+        private var path   = CGVector     (dx: 0, dy: 10)
 
         func fire (o: CGVector) -> SKSpriteNode {
-            sprite.name = "laserbeam"
-            sprite.physicsBody = SKPhysicsBody(rectangleOf: sprite.size)
-        
+            sprite.name                            = "laserbeam"
+            sprite.physicsBody                     = SKPhysicsBody(rectangleOf: sprite.size)
+            sprite.physicsBody?.categoryBitMask    = laserbeamCollisionCat
+            sprite.physicsBody?.contactTestBitMask = asteroidCollisionCat
+            sprite.physicsBody?.collisionBitMask   = asteroidCollisionCat
+            
             origin.dx = o.dx
             origin.dy = o.dy
             
@@ -34,7 +37,10 @@ extension GameScene {
         }
         
         func getPosition () -> CGVector {
-            return CGVector(dx: sprite.position.x, dy: sprite.position.y)
+            return CGVector (
+                dx: sprite.position.x,
+                dy: sprite.position.y
+            )
         }
         
         func cull () {
