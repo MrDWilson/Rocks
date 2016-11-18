43c24b820087dfb65f8cc13105af9eb102db7240
Ryan Needham <RyanNeedham@outlook.com>
2016-11-18 08:14:50 +0000

    Moved gameplay ship physics to player class to resolve some collision issues.

--- Rocks/Ship.swift
+++ Rocks/Ship.swift
@@ -10,26 +10,26 @@
 import SpriteKit
 
 extension GameScene {
+
     class Ship: SKSpriteNode {
-        
-        private let dstSprite_LeftWing  = SKSpriteNode ()
-        private var leftWingVelocity    = CGVector     (dx: -4, dy: 1)
-        private let dstSprite_RightWing = SKSpriteNode ()
-        private var rightWingVelocity   = CGVector     (dx: 4, dy: 1)
-        private let dstSprite_FrontBody = SKSpriteNode ()
-        private var frontBodyVelocity   = CGVector     (dx: 1, dy: 4)
-        private let dstSprite_BackBody  = SKSpriteNode ()
-        private var backBodyVelocity    = CGVector     (dx: -1, dy: 4)
-        private let explosionEffect     = SKEmitterNode(fileNamed: "ExplosionParticle.sks")
-        private var thrusterEffect      = SKEmitterNode(fileNamed: "ThrusterParticle_" + String(describing: (1 + arc4random_uniform(7))) + ".sks")
-        private var colourChangeArray   = [SKAction]()
-        
+        private let leftWing          = SKSpriteNode ()
+        private var leftWingVelocity  = CGVector     (dx: -4, dy: 1)
+        private let rightWing         = SKSpriteNode ()
+        private var rightWingVelocity = CGVector     (dx: 4, dy: 1)
+        private let frontBody         = SKSpriteNode ()
+        private var frontBodyVelocity = CGVector     (dx: 1, dy: 4)
+        private let backBody          = SKSpriteNode ()
+        private var backBodyVelocity  = CGVector     (dx: -1, dy: 4)
+        private let explosionEffect   = SKEmitterNode(fileNamed: "ExplosionParticle.sks")
+        private var thrusterEffect    = SKEmitterNode(fileNamed: "ThrusterParticle_" + String(describing: (1 + arc4random_uniform(7))) + ".sks")
+        private var colourChangeArray = [SKAction]()
+
         // admin
-        private var bodyID: Int!
+        private var bodyID:     Int!
         private var thrusterID: Int!
-        private var colorID: UIColor!
-        private var exploding = false
-        
+        private var colorID:    UIColor!
+        private var exploding   = false
+
         init (bID: Int, tID: Int, cID: UIColor) {
             super.init(texture: SKTexture(imageNamed: "Ship_" + String(describing: bID)), color: cID, size: CGSize(width: 24, height: 24))
             
@@ -40,61 +40,57 @@ extension GameScene {
             /* * * * * * * * * * * *
              *  Destruction Stuff
              * * * * * * * * * * * */
-            dstSprite_LeftWing.name                             = "ShipPart"
-            dstSprite_LeftWing.texture                          = SKTexture(imageNamed: "Ship_leftWing")
-            dstSprite_LeftWing.size.width                       = 12
-            dstSprite_LeftWing.size.height                      = 40
-            dstSprite_LeftWing.position                         = CGPoint(x: -14, y: 0)
-            dstSprite_LeftWing.physicsBody                      = SKPhysicsBody(rectangleOf: dstSprite_LeftWing.size)
-            dstSprite_LeftWing.physicsBody?.isDynamic           = true
-            dstSprite_LeftWing.physicsBody?.categoryBitMask     = playerCollisionCat
-            dstSprite_LeftWing.physicsBody?.contactTestBitMask  = asteroidCollisionCat
-            dstSprite_LeftWing.physicsBody?.collisionBitMask    = asteroidCollisionCat
-            dstSprite_LeftWing.physicsBody!.allowsRotation      = true
-            dstSprite_LeftWing.physicsBody?.mass                = 0.12
-            dstSprite_LeftWing.run(SKAction.colorize(with: colorID, colorBlendFactor: 0.85, duration: 0))
+            leftWing.name                             = "ShipPart"
+            leftWing.texture                          = SKTexture(imageNamed: "Ship_leftWing")
+            leftWing.size.width                       = 12
+            leftWing.size.height                      = 40
+            leftWing.position                         = CGPoint(x: -14, y: 0)
+            leftWing.physicsBody                      = SKPhysicsBody(rectangleOf: leftWing.size)
+            leftWing.physicsBody?.isDynamic           = true
+            leftWing.physicsBody?.categoryBitMask     = playerCollisionCat
+            leftWing.physicsBody?.contactTestBitMask  = asteroidCollisionCat
+            leftWing.physicsBody?.collisionBitMask    = asteroidCollisionCat
+            leftWing.physicsBody!.allowsRotation      = true
+            leftWing.physicsBody?.mass                = 0.12
             
-            dstSprite_RightWing.name                            = "ShipPart"
-            dstSprite_RightWing.texture                         = SKTexture(imageNamed: "Ship_rightWing")
-            dstSprite_RightWing.size.width                      = 12
-            dstSprite_RightWing.size.height                     = 40
-            dstSprite_RightWing.position                        = CGPoint(x: 14, y: 0)
-            dstSprite_RightWing.physicsBody                     = SKPhysicsBody(rectangleOf: dstSprite_RightWing.size)
-            dstSprite_RightWing.physicsBody?.isDynamic          = true
-            dstSprite_RightWing.physicsBody?.categoryBitMask    = playerCollisionCat
-            dstSprite_RightWing.physicsBody?.contactTestBitMask = asteroidCollisionCat
-            dstSprite_RightWing.physicsBody?.collisionBitMask   = asteroidCollisionCat
-            dstSprite_RightWing.physicsBody!.allowsRotation     = true
-            dstSprite_RightWing.physicsBody?.mass               = 0.08
-            dstSprite_RightWing.run(SKAction.colorize(with: colorID, colorBlendFactor: 0.85, duration: 0))
+            rightWing.name                            = "ShipPart"
+            rightWing.texture                         = SKTexture(imageNamed: "Ship_rightWing")
+            rightWing.size.width                      = 12
+            rightWing.size.height                     = 40
+            rightWing.position                        = CGPoint(x: 14, y: 0)
+            rightWing.physicsBody                     = SKPhysicsBody(rectangleOf: rightWing.size)
+            rightWing.physicsBody?.isDynamic          = true
+            rightWing.physicsBody?.categoryBitMask    = playerCollisionCat
+            rightWing.physicsBody?.contactTestBitMask = asteroidCollisionCat
+            rightWing.physicsBody?.collisionBitMask   = asteroidCollisionCat
+            rightWing.physicsBody!.allowsRotation     = true
+            rightWing.physicsBody?.mass               = 0.08
             
-            dstSprite_FrontBody.name                            = "ShipPart"
-            dstSprite_FrontBody.texture                         = SKTexture(imageNamed: "Ship_bodyFront")
-            dstSprite_FrontBody.size.width                      = 10
-            dstSprite_FrontBody.size.height                     = 14
-            dstSprite_FrontBody.position                        = CGPoint(x: 0, y: 12)
-            dstSprite_FrontBody.physicsBody                     = SKPhysicsBody(rectangleOf: dstSprite_FrontBody.size)
-            dstSprite_FrontBody.physicsBody?.isDynamic          = true
-            dstSprite_FrontBody.physicsBody?.categoryBitMask    = playerCollisionCat
-            dstSprite_FrontBody.physicsBody?.contactTestBitMask = asteroidCollisionCat
-            dstSprite_FrontBody.physicsBody?.collisionBitMask   = asteroidCollisionCat
-            dstSprite_FrontBody.physicsBody!.allowsRotation     = true
-            dstSprite_FrontBody.physicsBody?.mass               = 0.2
-            dstSprite_FrontBody.run(SKAction.colorize(with: colorID, colorBlendFactor: 0.85, duration: 0))
+            frontBody.name                            = "ShipPart"
+            frontBody.texture                         = SKTexture(imageNamed: "Ship_bodyFront")
+            frontBody.size.width                      = 10
+            frontBody.size.height                     = 14
+            frontBody.position                        = CGPoint(x: 0, y: 12)
+            frontBody.physicsBody                     = SKPhysicsBody(rectangleOf: frontBody.size)
+            frontBody.physicsBody?.isDynamic          = true
+            frontBody.physicsBody?.categoryBitMask    = playerCollisionCat
+            frontBody.physicsBody?.contactTestBitMask = asteroidCollisionCat
+            frontBody.physicsBody?.collisionBitMask   = asteroidCollisionCat
+            frontBody.physicsBody!.allowsRotation     = true
+            frontBody.physicsBody?.mass               = 0.2
             
-            dstSprite_BackBody.name                             = "ShipPart"
-            dstSprite_BackBody.texture                          = SKTexture(imageNamed: "Ship_bodyBack")
-            dstSprite_BackBody.size.width                       = 20
-            dstSprite_BackBody.size.height                      = 20
-            dstSprite_BackBody.position                         = CGPoint(x: 0, y: -8)
-            dstSprite_BackBody.physicsBody                      = SKPhysicsBody(rectangleOf: dstSprite_BackBody.size)
-            dstSprite_BackBody.physicsBody?.isDynamic           = true
-            dstSprite_BackBody.physicsBody?.categoryBitMask     = playerCollisionCat
-            dstSprite_BackBody.physicsBody?.contactTestBitMask  = asteroidCollisionCat
-            dstSprite_BackBody.physicsBody?.collisionBitMask    = asteroidCollisionCat
-            dstSprite_BackBody.physicsBody!.allowsRotation      = true
-            dstSprite_BackBody.physicsBody?.mass                = 0.2
-            dstSprite_BackBody.run(SKAction.colorize(with: colorID, colorBlendFactor: 0.85, duration: 0))
+            backBody.name                             = "ShipPart"
+            backBody.texture                          = SKTexture(imageNamed: "Ship_bodyBack")
+            backBody.size.width                       = 20
+            backBody.size.height                      = 20
+            backBody.position                         = CGPoint(x: 0, y: -8)
+            backBody.physicsBody                      = SKPhysicsBody(rectangleOf: backBody.size)
+            backBody.physicsBody?.isDynamic           = true
+            backBody.physicsBody?.categoryBitMask     = playerCollisionCat
+            backBody.physicsBody?.contactTestBitMask  = asteroidCollisionCat
+            backBody.physicsBody?.collisionBitMask    = asteroidCollisionCat
+            backBody.physicsBody!.allowsRotation      = true
+            backBody.physicsBody?.mass                = 0.2
             
             /* * * * * * * * * * * *
              *  Gameplay stuff
@@ -105,18 +101,6 @@ extension GameScene {
             texture                                      = SKTexture(imageNamed: "Ship_1") // add spaceship texture to sprite
             size.width                                   = 42
             size.height                                  = 42
-            
-            // Give physics
-            physicsBody                                  = SKPhysicsBody(rectangleOf: size)
-            physicsBody?.isDynamic                       = true
-            physicsBody?.categoryBitMask                 = playerCollisionCat
-            physicsBody?.contactTestBitMask              = asteroidCollisionCat
-            physicsBody?.collisionBitMask                = asteroidCollisionCat
-            physicsBody!.affectedByGravity               = false
-            physicsBody!.mass                            = 0.02
-            physicsBody!.allowsRotation                  = false
-            physicsBody!.usesPreciseCollisionDetection   = true
-            physicsBody!.linearDamping                   = 3
             
             // Give Particle Effect
             explosionEffect?.name                               = String("explosion")
@@ -130,26 +114,30 @@ extension GameScene {
             addChild(thrusterEffect!)
             
             run(SKAction.colorize(with: colorID, colorBlendFactor: 0.85, duration: 0))
-            
+            leftWing.run(SKAction.colorize(with: colorID, colorBlendFactor: 0.85, duration: 0))
+            rightWing.run(SKAction.colorize(with: colorID, colorBlendFactor: 0.85, duration: 0))
+            frontBody.run(SKAction.colorize(with: colorID, colorBlendFactor: 0.85, duration: 0))
+            backBody.run(SKAction.colorize(with: colorID, colorBlendFactor: 0.85, duration: 0))
+
         }
         
         required init?(coder aDecoder: NSCoder) { fatalError("init(coder:) has not been implemented")}
-        
+    
         func update () {
-            
+    
             // pieces dispursal
             if (exploding) {
-                
+    
                 // apply velocity
-                dstSprite_LeftWing.position.x  += leftWingVelocity.dx
-                dstSprite_LeftWing.position.y  += leftWingVelocity.dy
-                dstSprite_RightWing.position.x += rightWingVelocity.dx
-                dstSprite_RightWing.position.y += rightWingVelocity.dy
-                dstSprite_FrontBody.position.x += frontBodyVelocity.dx
-                dstSprite_FrontBody.position.y += frontBodyVelocity.dy
-                dstSprite_BackBody.position.x  += backBodyVelocity.dx
-                dstSprite_BackBody.position.y  += backBodyVelocity.dy
-                
+                leftWing.position.x  += leftWingVelocity.dx
+                leftWing.position.y  += leftWingVelocity.dy
+                rightWing.position.x += rightWingVelocity.dx
+                rightWing.position.y += rightWingVelocity.dy
+                frontBody.position.x += frontBodyVelocity.dx
+                frontBody.position.y += frontBodyVelocity.dy
+                backBody.position.x  += backBodyVelocity.dx
+                backBody.position.y  += backBodyVelocity.dy
+    
                 // dampen
                 leftWingVelocity.dx  *= 0.98
                 leftWingVelocity.dy  *= 0.98
@@ -159,7 +147,7 @@ extension GameScene {
                 frontBodyVelocity.dy *= 0.98
                 backBodyVelocity.dx  *= 0.98
                 backBodyVelocity.dy  *= 0.98
-                
+    
                 // keep parts moving forward
                 leftWingVelocity.dy  += 0.04
                 rightWingVelocity.dy += 0.04
@@ -167,7 +155,7 @@ extension GameScene {
                 backBodyVelocity.dy  += 0.04
             }
         }
-        
+    
         func explode () {
             exploding = true
             
@@ -183,33 +171,33 @@ extension GameScene {
             
             // show the pieces
             resetFragmentedPieces()
-            parent?.addChild(dstSprite_LeftWing)
-            parent?.addChild(dstSprite_RightWing)
-            parent?.addChild(dstSprite_BackBody)
-            parent?.addChild(dstSprite_FrontBody)
+            parent?.addChild(leftWing)
+            parent?.addChild(rightWing)
+            parent?.addChild(backBody)
+            parent?.addChild(frontBody)
             
-            dstSprite_LeftWing.physicsBody?.applyImpulse  (leftWingVelocity,  at: position)
-            dstSprite_RightWing.physicsBody?.applyImpulse (rightWingVelocity, at: position)
-            dstSprite_FrontBody.physicsBody?.applyImpulse (frontBodyVelocity, at: position)
-            dstSprite_BackBody.physicsBody?.applyImpulse  (backBodyVelocity,  at: position)
+            leftWing.physicsBody?.applyImpulse  (leftWingVelocity,  at: position)
+            rightWing.physicsBody?.applyImpulse (rightWingVelocity, at: position)
+            frontBody.physicsBody?.applyImpulse (frontBodyVelocity, at: position)
+            backBody.physicsBody?.applyImpulse  (backBodyVelocity,  at: position)
         }
         
         func isExploding () -> Bool {
             return exploding
         }
-        
+    
         func reassemble () {
-            
+        
             // remove colour
             colourChangeArray.removeAll()
             run(SKAction.colorize(with: colorID, colorBlendFactor: 0.85, duration: 0))
-            
+        
             // reassemble pieces
-            dstSprite_LeftWing.removeFromParent  ()
-            dstSprite_RightWing.removeFromParent ()
-            dstSprite_FrontBody.removeFromParent ()
-            dstSprite_BackBody.removeFromParent  ()
-            
+            leftWing.removeFromParent  ()
+            rightWing.removeFromParent ()
+            frontBody.removeFromParent ()
+            backBody.removeFromParent  ()
+    
             resetFragmentedVelocities()
             
             // kill explosion and start thrust
@@ -218,7 +206,7 @@ extension GameScene {
             // randomise thruster (CACHE THESE?)
             thrusterEffect = SKEmitterNode(fileNamed: "ThrusterParticle_" + String(describing: (1 + arc4random_uniform(7))) + ".sks")
             addChild(thrusterEffect!)
-            
+    
             isHidden = false
         }
         
@@ -235,6 +223,14 @@ extension GameScene {
             }
         }
         
+        func nextColour () {
+            
+        }
+        
+        func nextThruster () {
+            thrusterID = thrusterID + 1 % 7
+        }
+    
         private func resetFragmentedVelocities () {
             leftWingVelocity.dx  = -4
             leftWingVelocity.dy  = 1
@@ -245,16 +241,16 @@ extension GameScene {
             backBodyVelocity.dx  = -1
             backBodyVelocity.dy  = 4
         }
-        
+    
         private func resetFragmentedPieces () {
-            dstSprite_LeftWing.position.x  = position.x - 16
-            dstSprite_LeftWing.position.y  = position.y - 1
-            dstSprite_RightWing.position.x = position.x + 16
-            dstSprite_RightWing.position.y = position.y - 1
-            dstSprite_FrontBody.position.x = position.x
-            dstSprite_FrontBody.position.y = position.y + 10
-            dstSprite_BackBody.position.x  = position.x
-            dstSprite_BackBody.position.y  = position.y - 8
+            leftWing.position.x  = position.x - 16
+            leftWing.position.y  = position.y - 1
+            rightWing.position.x = position.x + 16
+            rightWing.position.y = position.y - 1
+            frontBody.position.x = position.x
+            frontBody.position.y = position.y + 10
+            backBody.position.x  = position.x
+            backBody.position.y  = position.y - 8
         }
     }
 }
