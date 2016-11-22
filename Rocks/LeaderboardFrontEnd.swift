a4cbf9227164d8fe7df20ee5a30b8dae2bf165a3
user <user@users-MacBook-Pro-3.local>
2016-11-22 19:29:13 +0000

    Fixed front end

--- Rocks/LeaderboardFrontEnd.swift
+++ Rocks/LeaderboardFrontEnd.swift
@@ -156,7 +156,7 @@ extension GameScene {
             print("This best be called I swear I will 1v1 wreck you swift")
             
             entries = backend.getEntries()
-           // player = backend.getPlayer()
+            player = backend.getPlayer()
             
             entries.forEach {
                 //let shipVector = Vector3D($0.context)
@@ -173,18 +173,18 @@ extension GameScene {
                     )
                 }
             }
-            /*
+            
             addChild (
                 LeaderboardEntry (
                     rank:     player.rank,
-                    username: player!.alias!,
-                    score:    $0.value,
+                    username: String("You"),
+                    score:    player.value,
                     ship:     Ship(bID: 1, tID: (1 + Int(arc4random_uniform(7))), cID: Int(arc4random_uniform(UInt32(REColour.COLOUR_BOUNDRY.rawValue)))), // THIS NEEDS USER SHIP COMPATABILITY
                     w:        CGFloat(w),
                     h:        CGFloat(h)
                 )
             )
-            */
+            
         }
     }
 }
